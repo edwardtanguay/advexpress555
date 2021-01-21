b150dd3bfc46488ec52eaaff7369a320beeeb90d
Edward Tanguay (wk) <edward@tanguay.info>
2021-01-21 10:55:57 +0100

    routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
 
 module.exports = (config) => {
 	const app = express();
 
-	app.use('/', express.static(path.join(__dirname, '../public')));
+	// app.use('/', express.static(path.join(__dirname, '../public')));
+
+	app.use('/', routes());
 
 	return app;
 }
